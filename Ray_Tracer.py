6c55fb721c07e38fff1ce68ab82614c6e86cda63
Amit Gabay <amitgabay9142@gmail.com>
2022-04-27 20:25:48 +0300

    parse scene

--- Ray_Tracer.py
+++ Ray_Tracer.py
@@ -1,85 +1,5 @@
 import sys
 
-def main(scene_path, output_path, img_width=500, img_height=500):
-    scene = parse_scene(scene_path)
-
-
-def parse_scene(scene_path):
-    with open(scene_path, 'rb') as f:
-        content = f.read()
-
-    material_list = []
-    sphere_list = []
-    plane_list = []
-    light_list = []
-    box_list = []
-    camera = None
-    settings = None
-    for line in content.splitlines():
-        if len(line) == 0 or line[0] == '#':
-            pass
-        line = line.split()
-        obj_name = line[0]
-
-        if obj_name == 'cam':
-            pos = (line[1], line[2], line[3])
-            look_up = (line[4], line[5], line[6])
-            up_vector = (line[7], line[8], line[9])
-            screen_dist = line[10]
-            screen_width = line[11]
-            camera = Camera(pos, look_up, up_vector, screen_dist, screen_width)
-
-        elif obj_name == 'set':
-            bg_color = (line[1], line[2], line[3])
-            N = line[4]
-            max_recursion = line[5]
-            settings = Settings(bg_color, N, max_recursion)
-
-        elif obj_name == 'lgt':
-            pos = (line[1], line[2], line[3])
-            color = (line[4], line[5], line[6])
-            specular_intens = line[7]
-            shadow_intens = line[8]
-            light_radius = line[9]
-            light_list.append(Light(pos, color, specular_intens, shadow_intens, light_radius))
-
-        elif obj_name == 'sph':
-            center_pos = (line[1], line[2], line[3])
-            radius = line[4]
-            material_idx = line[5]
-            sphere_list.append(Sphere(center_pos, radius, material_idx))
-
-        elif obj_name == 'pln':
-            normal_vector = (line[1], line[2], line[3])
-            offset = line[4]
-            material_idx = line[5]
-            plane_list.append(Plane(normal_vector, offset, material_idx))
-
-        elif obj_name == 'box':
-            center_pos = (line[1], line[2], line[3])
-            edge_len = line[4]
-            material_idx = line[5]
-            box_list.append(Box(center_pos, edge_len, material_idx))
-
-        elif obj_name == 'mtl':
-            diffuse_col = (line[1], line[2], line[3])
-            specular_col = (line[4], line[5], line[6])
-            reflection_col = (line[7], line[8], line[9])
-            phong_coeff = line[10]
-            transparent_val = line[11]
-            material_list.append(Material(diffuse_col, specular_col, reflection_col, phong_coeff, transparent_val))
-
-    scene = Scene(camera, settings, sphere_list, plane_list, box_list, light_list, material_list)
-    return scene
-
-
-if __name__ == '__main__':
-    if len(sys.argv) == 3:
-        main(sys.argv[1], sys.argv[2])
-
-    if len(sys.argv) == 5:
-        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
-
 
 class Scene:
     def __init__(self, camera, settings, sphere_list, plane_list, box_list, light_list, material_list):
@@ -147,16 +67,82 @@ class Light:
         self.light_radius = light_radius
 
 
+def parse_scene(scene_path):
+    with open(scene_path, 'rb') as f:
+        content = f.read()
+
+    material_list = []
+    sphere_list = []
+    plane_list = []
+    light_list = []
+    box_list = []
+    camera = None
+    settings = None
+    for line in content.splitlines():
+        if len(line) == 0 or line[0] == b'#':
+            continue
+        line = line.split()
+        obj_name = line[0]
+
+        if obj_name == b'cam':
+            pos = (float(line[1]), float(line[2]), float(line[3]))
+            look_up = (float(line[4]), float(line[5]), float(line[6]))
+            up_vector = (float(line[7]), float(line[8]), float(line[9]))
+            screen_dist = float(line[10])
+            screen_width = float(line[11])
+            camera = Camera(pos, look_up, up_vector, screen_dist, screen_width)
+
+        elif obj_name == b'set':
+            bg_color = (float(line[1]), float(line[2]), float(line[3]))
+            N = float(line[4])
+            max_recursion = float(line[5])
+            settings = Settings(bg_color, N, max_recursion)
+
+        elif obj_name == b'lgt':
+            pos = (float(line[1]), float(line[2]), float(line[3]))
+            color = (float(line[4]), float(line[5]), float(line[6]))
+            specular_intens = float(line[7])
+            shadow_intens = float(line[8])
+            light_radius = float(line[9])
+            light_list.append(Light(pos, color, specular_intens, shadow_intens, light_radius))
+
+        elif obj_name == b'sph':
+            center_pos = (float(line[1]), float(line[2]), float(line[3]))
+            radius = float(line[4])
+            material_idx = float(line[5])
+            sphere_list.append(Sphere(center_pos, radius, material_idx))
+
+        elif obj_name == b'pln':
+            normal_vector = (float(line[1]), float(line[2]), float(line[3]))
+            offset = float(line[4])
+            material_idx = float(line[5])
+            plane_list.append(Plane(normal_vector, offset, material_idx))
+
+        elif obj_name == b'box':
+            center_pos = (float(line[1]), float(line[2]), float(line[3]))
+            edge_len = float(line[4])
+            material_idx = float(line[5])
+            box_list.append(Box(center_pos, edge_len, material_idx))
+
+        elif obj_name == b'mtl':
+            diffuse_col = (float(line[1]), float(line[2]), float(line[3]))
+            specular_col = (float(line[4]), float(line[5]), float(line[6]))
+            reflection_col = (float(line[7]), float(line[8]), float(line[9]))
+            phong_coeff = float(line[10])
+            transparent_val = float(line[11])
+            material_list.append(Material(diffuse_col, specular_col, reflection_col, phong_coeff, transparent_val))
+
+    scene = Scene(camera, settings, sphere_list, plane_list, box_list, light_list, material_list)
+    return scene
 
 
+def main(scene_path, output_path, img_width=500, img_height=500):
+    scene = parse_scene(scene_path)
 
 
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        main(sys.argv[1], sys.argv[2])
 
-
-
-
-
-
-
-
-
+    if len(sys.argv) == 5:
+        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
