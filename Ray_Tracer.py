01724b6c1ecdc1dd0ef9677d4a7463f1f82a8c38
Liron Cohen <lironcohen3@mail.tau.ac.il>
2022-05-05 22:17:47 +0300

    Added py file

--- Ray_Tracer.py
+++ Ray_Tracer.py
@@ -1,92 +1,8 @@
-import os
 import sys
 import numpy as np
 import random
+from Classes import *
 from PIL import Image
-
-
-class Scene:
-    def __init__(self, camera, settings, sphere_list, plane_list, box_list, light_list, material_list):
-        self.camera = camera
-        self.settings = settings
-        self.sphere_list = sphere_list
-        self.plane_list = plane_list
-        self.box_list = box_list
-        self.light_list = light_list
-        self.material_list = material_list
-
-
-class Camera:
-    def __init__(self, pos, look_at, up_vector, screen_dist, screen_width, screen_height):
-        self.pos = pos
-        self.look_at = look_at
-        self.towards = look_at - pos
-        self.up_vector = up_vector
-        self.screen_dist = screen_dist
-        self.screen_width = screen_width
-        self.screen_height = screen_height
-
-
-class Settings:
-    def __init__(self, bg_color, N, max_recursion):
-        self.bg_color = bg_color
-        self.N = N
-        self.max_recursion = max_recursion
-
-
-class Material:
-    def __init__(self, diffuse_color, specular_color, reflection_color, phong_coeff, transparent_val):
-        self.diffuse_color = diffuse_color
-        self.specular_color = specular_color
-        self.reflection_color = reflection_color
-        self.phong_coeff = phong_coeff
-        self.transparent_val = transparent_val
-
-
-class Plane:
-    def __init__(self, normal_vector, offset, material_idx):
-        self.normal_vector = normal_vector
-        self.offset = offset
-        self.material_idx = material_idx
-
-
-class Sphere:
-    def __init__(self, center_pos, radius, material_idx):
-        self.center_pos = center_pos
-        self.radius = radius
-        self.material_idx = material_idx
-
-
-class Box:
-    def __init__(self, center_pos, edge_len, material_idx):
-        self.center_pos = center_pos
-        self.edge_len = edge_len
-        self.material_idx = material_idx
-
-
-class Light:
-    def __init__(self, pos, color, specular_intens, shadow_intens, light_radius):
-        self.pos = pos
-        self.color = color
-        self.specular_intens = specular_intens
-        self.shadow_intens = shadow_intens
-        self.light_radius = light_radius
-
-
-class Ray:
-    def __init__(self, origin, direction):
-        self.origin = origin
-        self.direction = direction
-
-    def compute_point(self, scalar):
-        return self.origin + scalar * self.direction
-
-
-class Screen:
-    def __init__(self, corner_pixel, horizontal, vertical):
-        self.corner_pixel = corner_pixel
-        self.horizontal = horizontal
-        self.vertical = vertical
 
 
 def parse_scene(scene_path, asp_ratio):
@@ -159,6 +75,62 @@ def parse_scene(scene_path, asp_ratio):
     return scene
 
 
+def represent_screen(camera, width_pixels, height_pixels):
+    # Determine screen's horizontal, vertical vectors:
+    horizontal = np.cross(camera.towards, camera.up_vector)
+    horizontal = horizontal / np.linalg.norm(horizontal)
+    vertical = np.cross(horizontal, camera.towards)
+    vertical = vertical / np.linalg.norm(vertical)
+    # Determine screen's leftmost bottom pixel (corner pixel):
+    left_bottom_pixel = camera.towards * camera.screen_dist - camera.screen_width/2 * horizontal - camera.screen_height/2 * vertical
+    # Normalize screen's horizontal, vertical vectors by pixel's width / height:
+    pixel_width = camera.screen_width / width_pixels
+    pixel_height = camera.screen_height / height_pixels
+    horizontal = pixel_width * horizontal
+    vertical = pixel_height * vertical
+    # Align 'left_bottom_pixel' to the pixel's center:
+    left_bottom_pixel += 0.5*horizontal + 0.5*vertical
+    # Represent the screen:
+    screen = Screen(left_bottom_pixel, horizontal, vertical)
+    return screen
+
+
+def construct_pixel_ray(camera, screen, i, j):
+    pixel_center = screen.corner_pixel + i * screen.horizontal + j * screen.vertical
+    ray_vector = pixel_center - camera.pos
+    pixel_ray = Ray(camera.pos, ray_vector)
+    return pixel_ray
+    #towards = camera.look_at - camera.pos
+    #up = camera.up_vector
+    #right = np.cross(towards, up)
+    #P0 = camera.pos
+    #d = camera.screen_dist
+    #w = camera.screen_width
+    #h = camera.screen_height
+
+    #P_left = P0 + d * towards - (w * right) / 2
+    #P_down = P0 + d * towards - (h * up) / 2
+    #P = P_left + (j/w + 0.5) * w * right + P_down + (i/h + 0.5) * h * up
+    #V = (P-P0) / np.linalg.norm(P-P0)
+    #return V
+
+
+def ray_casting(scene, img_width, img_height):
+    # TODO: understand if we need to change the matrix coordinate (end of lecture 5)
+    image = np.zeros((img_width, img_height, 3), dtype=float)
+    screen = represent_screen(scene.camera, img_width, img_height)
+    for i in range(img_width):
+        for j in range(img_height):
+            ray = construct_pixel_ray(scene.camera, screen, i, j)
+            surface, min_intersect = find_min_intersect(scene, ray)
+            basic_color = calc_surface_color(scene, surface, min_intersect)
+            is_lit = trace_light_rays(scene, surface)
+            soft_shadow = produce_soft_shadow(scene.surface)
+            output_color = calc_output_color()
+            image[i, j] = output_color
+    save_image(image, output_path)
+
+
 def find_sphere_intersect(scene, ray, sphere):
     O = sphere.center_pos
     P0 = scene.camera.pos
@@ -216,95 +188,6 @@ def find_min_intersect(scene, ray):
     return min_surface, min_intersect
 
 
-def save_image(image, output_path):
-    img = Image.fromarray(image)
-    img.save(output_path)
-
-
-def construct_pixel_ray(camera, screen, i, j):
-    pixel_center = screen.corner_pixel + i * screen.horizontal + j * screen.vertical
-    ray_vector = pixel_center - camera.pos
-    pixel_ray = Ray(camera.pos, ray_vector)
-    return pixel_ray
-    #towards = camera.look_at - camera.pos
-    #up = camera.up_vector
-    #right = np.cross(towards, up)
-    #P0 = camera.pos
-    #d = camera.screen_dist
-    #w = camera.screen_width
-    #h = camera.screen_height
-
-    #P_left = P0 + d * towards - (w * right) / 2
-    #P_down = P0 + d * towards - (h * up) / 2
-    #P = P_left + (j/w + 0.5) * w * right + P_down + (i/h + 0.5) * h * up
-    #V = (P-P0) / np.linalg.norm(P-P0)
-    #return V
-
-
-def represent_screen(camera, width_pixels, height_pixels):
-    # Determine screen's horizontal, vertical vectors:
-    horizontal = np.cross(camera.towards, camera.up_vector)
-    horizontal = horizontal / np.linalg.norm(horizontal)
-    vertical = np.cross(horizontal, camera.towards)
-    vertical = vertical / np.linalg.norm(vertical)
-    # Determine screen's leftmost bottom pixel (corner pixel):
-    left_bottom_pixel = camera.towards * camera.screen_dist - camera.screen_width/2 * horizontal - camera.screen_height/2 * vertical
-    # Normalize screen's horizontal, vertical vectors by pixel's width / height:
-    pixel_width = camera.screen_width / width_pixels
-    pixel_height = camera.screen_height / height_pixels
-    horizontal = pixel_width * horizontal
-    vertical = pixel_height * vertical
-    # Align 'left_bottom_pixel' to the pixel's center:
-    left_bottom_pixel += 0.5*horizontal + 0.5*vertical
-    # Represent the screen:
-    screen = Screen(left_bottom_pixel, horizontal, vertical)
-    return screen
-
-
-def calc_surface_color(scene, surface, min_intersect):
-    bg_col = scene.settings.bg_color
-    trans_val = (scene.material_list[surface.material_idx]).transparent_val
-    diffuse_col = (scene.material_list[surface.material_idx]).diffuse_color
-    specular_col = (scene.material_list[surface.material_idx]).specular_color
-    reflection_col = (scene.material_list[surface.material_idx]).reflection_color
-
-    normal = calc_surface_normal(surface, min_intersect)
-    normal /= np.linalg.norm(normal)
-    for light in scene.light_list:
-        # Calc light effect on diffuse color
-        light_direction = min_intersect - light.pos
-        soft_shadow_fraction = soft_shadow_fraction(scene, scene.settings.N, light, min_intersect)
-        light_intensity = (1 - light.shadow_intens) + (light.shadow_intens * soft_shadow_fraction)
-        diffuse_col *= light_intensity * normal.dot(light_direction)
-
-        # Calc light effect on specular color
-        L = np.array(min_intersect - light.pos)
-        R = L - 2 * (L.dot(normal)) * normal
-        V = np.array(scene.camera.pos - min_intersect)
-        surface_matrial = scene.material_list[surface.material_idx]
-        n = surface_matrial.phong_coeff
-        specular_col *= light_intensity * ((R.dot(V))**n)
-
-    output_color = bg_col * trans_val + (diffuse_col + specular_col) * (1 - trans_val) + reflection_col
-    return output_color
-
-
-def ray_casting(scene, img_width, img_height):
-    # TODO: understand if we need to change the matrix coordinate (end of lecture 5)
-    image = np.zeros((img_width, img_height, 3), dtype=float)
-    screen = represent_screen(scene.camera, img_width, img_height)
-    for i in range(img_width):
-        for j in range(img_height):
-            ray = construct_pixel_ray(scene.camera, screen, i, j)
-            surface, min_intersect = find_min_intersect(scene, ray)
-            basic_color = calc_surface_color(scene, surface, min_intersect)
-            is_lit = trace_light_rays(scene, surface)
-            soft_shadow = produce_soft_shadow(scene.surface)
-            output_color = calc_output_color()
-            image[i, j] = output_color
-    save_image(image, output_path)
-
-
 def calc_surface_normal(surface, min_intersect):
     if type(surface) == Sphere:
         return np.array(min_intersect - surface.sphere.center_pos)
@@ -316,7 +199,7 @@ def calc_surface_normal(surface, min_intersect):
         pass # TODO fill
 
 
-def soft_shadow_fraction(scene, N, light, min_intersect):
+def calc_soft_shadow_fraction(scene, N, light, min_intersect):
     light_ray_direct = min_intersect - light.pos
     light_ray_direct /= np.linalg.norm(light_ray_direct)
 
@@ -347,6 +230,39 @@ def soft_shadow_fraction(scene, N, light, min_intersect):
             if cell_min_intersect == min_intersect:
                 intersect_counter += 1
     return intersect_counter / (N * N)
+
+
+def calc_surface_color(scene, surface, min_intersect):
+    bg_col = scene.settings.bg_color
+    trans_val = (scene.material_list[surface.material_idx]).transparent_val
+    diffuse_col = (scene.material_list[surface.material_idx]).diffuse_color
+    specular_col = (scene.material_list[surface.material_idx]).specular_color
+    reflection_col = (scene.material_list[surface.material_idx]).reflection_color
+
+    normal = calc_surface_normal(surface, min_intersect)
+    normal /= np.linalg.norm(normal)
+    for light in scene.light_list:
+        # Calc light effect on diffuse color
+        light_direction = min_intersect - light.pos
+        soft_shadow_fraction = calc_soft_shadow_fraction(scene, scene.settings.N, light, min_intersect)
+        light_intensity = (1 - light.shadow_intens) + (light.shadow_intens * soft_shadow_fraction)
+        diffuse_col *= light_intensity * normal.dot(light_direction)
+
+        # Calc light effect on specular color
+        L = np.array(min_intersect - light.pos)
+        R = L - 2 * (L.dot(normal)) * normal
+        V = np.array(scene.camera.pos - min_intersect)
+        surface_matrial = scene.material_list[surface.material_idx]
+        n = surface_matrial.phong_coeff
+        specular_col *= light_intensity * ((R.dot(V))**n)
+
+    output_color = bg_col * trans_val + (diffuse_col + specular_col) * (1 - trans_val) + reflection_col
+    return output_color
+
+
+def save_image(image, output_path):
+    img = Image.fromarray(image)
+    img.save(output_path)
 
 
 def main(scene_path, output_path, img_width=500, img_height=500):
